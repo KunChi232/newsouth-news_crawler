0bf11f5f7b10cdbde77f2456a6945914752b0903
KunChi232 <kuo23201199@gmail.com>
2020-08-23 00:46:19 +0800

    temp

--- crawler.py
+++ crawler.py
@@ -5,6 +5,7 @@ import sys, time ,argparse
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from .functions import *
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
@@ -14,30 +15,31 @@ options.add_argument("disable-gpu")
 parser = argparse.ArgumentParser()
 parser.add_argument('--target', type = str, required = True)
 parser.add_argument('--keywords', type = str, requests = True)
-parser.add_argument('--count', type=int, requests = True)
+parser.add_argument('--counts', type=int, requests = True)
 
 
 
 
-def crawler(target):
+def crawler(target, keywords, counts):
     if(target == 'https://www.who.int/southeastasia'):
-        pass
+        crawlWhoSoutheastasia(keywords, counts)
     elif(target == 'https://www.who.int/westernpacific'):
-        pass
+        crawlWhoWesternpacific(keywords,counts)
     elif(target == 'https://www.psychiatry.org/'):
-        pass
+        crawlPsychiatry(keywords, counts)
     elif(target == 'https://timesofindia.indiatimes.com/'):
-        pass
+        crawlTimesofindia(keywords, counts)
     elif(target == 'https://www.thejakartapost.com/'):
-        pass
+        crawlThejakartapost(keywords, counts)
     elif(target == 'https://www.bangkokpost.com/'):
-        pass
+        crawlBangkokpost(keywords, counts)
     else:
         print('Target website is not available')
 
 def main():
     args = parser.parse_args()
-
+    crawler(args.targe, args.keywords, args.counts)
+    
 if __name__ == '__main__' :
     main()
 
